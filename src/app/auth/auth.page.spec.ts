314e961d2a6f13f21da0c4c8a94a5942adffd40a
Yae <alexiayaeldemaio@gmail.com>
2023-05-12 12:17:36 +0200

    test-auth page

--- src/app/auth/auth.page.spec.ts
+++ src/app/auth/auth.page.spec.ts
@@ -1,24 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthPage } from './auth.page';
+import { Router } from '@angular/router';
 
 describe('AuthPage', () => {
   let component: AuthPage;
   let fixture: ComponentFixture<AuthPage>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
+    TestBed.configureTestingModule({
+      declarations: [ AuthPage ],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+      ],
+    });
     fixture = TestBed.createComponent(AuthPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should emit goLogin event when button is clicked', () => {
-    spyOn(component, 'goLogin');
-    const button = fixture.nativeElement.querySelector('ion-button');
-    button.click();
-    expect(component.goLogin).toHaveBeenCalled();
+  it('should navigate to login with "login" url', () => {
+    component.goLogin('login')
+    expect(routerSpy.navigateByUrl).toHaveBeenCalled();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith(`auth/login`);
+  });
+
+  it('should navigate to login with "login-layout" url', () => {
+    component.goLogin('login-layout')
+    expect(routerSpy.navigateByUrl).toHaveBeenCalled();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith(`auth/login-layout`);
   });
 });
